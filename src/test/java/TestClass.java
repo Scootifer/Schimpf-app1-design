8a278229d9a5614b4f948a3f161f3a841983140d
Scootifer <Windmill3341!>
2021-10-24 21:34:57 -0400

    test psuedocode addition

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,33 @@
+/*
+ *  UCF COP3330 Summer 2021 Application Assignment 1 Solution
+ *  Copyright 2021 Scott Schimpf
+ */
+
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+
+    //This test will attempt to load a save file and test the values of the created object
+    @Test
+    void testLoad() {
+
+    }
+
+    //This test will attempt to save a file and will compare it to an accurate output file to test.
+    @Test
+    void testSave() {
+
+    }
+
+    //This test will test the ability to sort the list
+    @Test
+    void testSortComplete() {
+
+    }
+
+    //This test will the test ability to sort the list
+    @Test
+    void testSortIncomplete() {
+
+    }
+}
